7e35156fbb601539b4035f8c51bc3cad2deb2673
AmbujamSivan <42361195+AmbujamSivan@users.noreply.github.com>
2019-01-24 10:51:28 -0800

    Added PUT,PATCH,DELETE,POSt methods to the API view

--- DjangoRestApi/src/DjangoWeb_Project/RestApi_App/views.py
+++ DjangoRestApi/src/DjangoWeb_Project/RestApi_App/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from . import serializer
 
 # Create your views here.
 class HelloApiView(APIView):
     #To Test APIView
+    """Tells taht we are going to use this helloserializer for this APIView"""
+    serializer_class = serializer.HelloSerializer
 
     def get(self, request, format=None):
         """Return the list of API features"""
@@ -16,3 +21,27 @@ class HelloApiView(APIView):
         ]
         #Response Objj should be returned as a Dictionary which is then converted to JSON
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Returns a variable of our name"""
+
+        serialized = serializer.HelloSerializer(data = request.data)
+
+        if serialized.is_valid():
+            name = serialized.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message':message})
+        else:
+            return Response(serialized.errors, status = status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        """ performs Update functionality"""
+        return Response({'method':'put'})
+
+    def patch(self, request, pk = None):
+        """ partial updation of the object only those provided in the req"""
+        return Response({'method':'patch'})
+
+    def delete(self, request, pk = None):
+        """Perfors the deletion of object"""
+        return Response({'method':'delete'})
