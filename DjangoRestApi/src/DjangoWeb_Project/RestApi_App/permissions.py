5adfe53c618be901ae8fb0d30de3483839355bfb
AmbujamSivan <42361195+AmbujamSivan@users.noreply.github.com>
2019-01-25 12:25:09 -0800

    Added the search filter

--- DjangoRestApi/src/DjangoWeb_Project/RestApi_App/permissions.py
+++ DjangoRestApi/src/DjangoWeb_Project/RestApi_App/permissions.py
@@ -2,15 +2,12 @@ from rest_framework import permissions
 
 class UpdateOwnProfile(permissions.BasePermission):
     """Allows user to edit their own profile"""
-    """
-        Object-level permission to only allow owners of an object to edit it.
-        Assumes the model instance has an `owner` attribute.      """
+    """ Object-level permission to only allow owners of an object to edit it."""
 
     def has_object_permission(self, request, view, obj):
         # Read permissions are allowed to any request,
         # so we'll always allow GET, HEAD or OPTIONS requests.
-        """ Check if it is their own profile"""
         if request.method in permissions.SAFE_METHODS:
             return True
-
+        #""" Check if it is their own profile"""
         return obj.id == request.user.id
