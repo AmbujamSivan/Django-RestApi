9dacf6b596a7dc12d0e84f4826c0104049640c2a
AmbujamSivan <42361195+AmbujamSivan@users.noreply.github.com>
2019-01-25 13:42:37 -0800

    Added the restriction to see feeds for authenticated user ony

--- DjangoRestApi/src/DjangoWeb_Project/RestApi_App/views.py
+++ DjangoRestApi/src/DjangoWeb_Project/RestApi_App/views.py
@@ -9,6 +9,7 @@ from rest_framework import filters
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 
 from . import serializer
 from . import permissions
@@ -125,7 +126,7 @@ class UserProfileFeedViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     serializer_class = serializer.ProfileFeedItemSerializer
     queryset = models.ProfileFeedItem.objects.all()
-    permission_classes =(permissions.PostOwnStatus, IsAuthenticatedOrReadOnly)
+    permission_classes =(permissions.PostOwnStatus, IsAuthenticated)
 
     def perform_create(self, serializer):
         """sets the feed to the logged in user"""
